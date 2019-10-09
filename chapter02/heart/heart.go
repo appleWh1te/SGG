f82987e4b6d36f0ae001d3c5bfd00540f31f94d2
appleWh1te <handsomeyiisme@gmail.com>
2019-10-09 19:44:04 +0800

    add heart.go instance

---
+++ chapter02/heart/heart.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("  *                   *  ")
+	fmt.Println("*   * I love Golang *   *")
+	fmt.Println("  *                   *  ")
+	fmt.Println("    *                *   ")
+	fmt.Println("      *            *     ")
+	fmt.Println("        *        *       ")
+	fmt.Println("          *    *         ")
+	fmt.Println("             *           ")
+}
